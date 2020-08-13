dcc978165ec19333f477790da17567206c851cb5
mkl <1162763504@qq.com>
2020-08-13 11:09:41 +0800

    微服务集群

--- cloud2020/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud2020/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -14,7 +14,8 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 
-    public static final String PAYMENT_URL = "http://localhost:8001";
+//    public static final String PAYMENT_URL = "http://localhost:8001";
+public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
@@ -26,7 +27,7 @@ public class OrderController {
 
     @GetMapping("/consumer/payment/get/{id}")
     public  CommonResult<Payment> getPayment(@PathVariable("id") Long id){
-        return restTemplate.getForObject(PAYMENT_URL+"payment/get/"+id,CommonResult.class);
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
 
 }
--- cloud2020/cloud-provider-payment8002/src/main/java/springcloud/controller/PaymentController.java
+++ cloud2020/cloud-provider-payment8002/src/main/java/springcloud/controller/PaymentController.java
@@ -1,11 +1,11 @@
-package com.atguigu.springcloud.controller;
+package springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
-import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import springcloud.service.PaymentService;
 
 import javax.annotation.Resource;
 
